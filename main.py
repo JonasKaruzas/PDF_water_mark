60f9eb5289c15da347e890e248a0bc69b4f7790d
JonasKaruzas <EnterGitHub1>
2021-08-28 17:02:35 +0300

    working

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hi')
+import PyPDF2
+
+pdf_w = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+pdf2 = PyPDF2.PdfFileReader(open('dummy.pdf', 'rb'))
+output = PyPDF2.PdfFileWriter()
+
+for i in range(pdf2.getNumPages()):
+    page = pdf2.getPage(i)
+    page.mergePage(pdf_w.getPage(0))
+    output.addPage(page)
+
+    with open('watermarked_output.pdf', 'wb') as file:
+        output.write(file)
